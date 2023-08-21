86bfbc78132286df7aa954b047b2b0853b8bea49
e-lec-tron <business.fahan@gmail.com>
2023-08-21 11:28:27 +0530

    jwt configs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenRefreshView
+)
+
 urlpatterns = [
     path('routes', views.getRoutes),
+    path('token/', views.MyTokenObtainPairView.as_view(), name = 'token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
 ]
