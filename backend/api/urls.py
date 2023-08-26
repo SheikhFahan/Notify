d6642998251a50a574bc2b3127985134332a2131
e-lec-tron <business.fahan@gmail.com>
2023-08-26 09:57:30 +0530

    url routing for downloads

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -12,4 +12,4 @@ urlpatterns = [
     path('routes', views.getRoutes),
     path('token/', views.MyTokenObtainPairView.as_view(), name = 'token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
